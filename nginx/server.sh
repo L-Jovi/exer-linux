8eda5df0789afdaa003a950f351f80652b0d148a
Eternal_Jovi <eternal.l.jovi@gmail.com>
2018-07-03 17:58:40 +0800

    Fix script.

--- nginx/server.sh
+++ nginx/server.sh
@@ -2,25 +2,32 @@
 
 config_path="/Users/jovi/github/private/exer-linux/nginx/nginx.conf"
 
+function show_config_path(){
+    echo "config file path >>> [ $config_path ]" | lolcat
+}
+
 function check_params(){
-    if [ $# -eq 0 ]
+    if [[ $# -eq 0 ]]
     then
+        show_config_path
         echo "start nginx service." | lolcat
         nginx -c $config_path
 
     else
-        if [[ $1 == "r" ]] || [[ $1 == "reload" ]]
+        if [[ $1 == "r" ]] || [[ $1 == "reload" ]] || [[ $1 == "-r" ]]
         then
+            show_config_path
             echo "reload nginx config..." | lolcat
             nginx -c $config_path -s reload
 
-        elif [[ $1 == "q" ]] || [[ $1 == "quit" ]]
+        elif [[ $1 == "q" ]] || [[ $1 == "quit" ]] || [[ $1 == "-q" ]]
         then
+            show_config_path
             echo "quit nginx service." | lolcat
             nginx -c $config_path -s quit
 
         else
-            echo "invalid directive [ $1 ]."
+            echo "invalid directive [ $1 ]." | lolcat
         fi
     fi
 }
